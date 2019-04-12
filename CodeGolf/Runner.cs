ce16d85d532d5456c553d63ae0e55f8897b4e98b
Matthew McCorry <mccorry@gmail.com>
2019-04-12 20:45:26 +0100

    a

--- CodeGolf/Runner.cs
+++ CodeGolf/Runner.cs
@@ -64,41 +64,54 @@ namespace CodeGolf
                  + "}";
         }
 
+        private static T UseTempFile<T>(Func<string> gen, Func<string, T> process)
+        {
+            var fileName = gen();
+
+            var res = process(fileName);
+            
+            File.Delete(fileName);
+
+            return res;
+        }
+
         private static Assembly Compile(string function)
         {
             var syntaxTree = CSharpSyntaxTree.ParseText(WrapInClass(function));
 
-            var assemblyName = Path.GetRandomFileName();
-            var references = new MetadataReference[]
+            return UseTempFile(Path.GetRandomFileName, assemblyName =>
             {
-                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
-            };
-
-            var compilation = CSharpCompilation.Create(
-                assemblyName,
-                syntaxTrees: new[] { syntaxTree },
-                references: references,
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-            using (var ms = new MemoryStream())
-            {
-                var result = compilation.Emit(ms);
-
-                if (!result.Success)
+                var references = new MetadataReference[]
                 {
-                    var failures = result.Diagnostics.Where(diagnostic =>
-                        diagnostic.IsWarningAsError ||
-                        diagnostic.Severity == DiagnosticSeverity.Error);
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+                };
 
-                    throw new Exception(string.Join(",\n", failures));
-                }
-                else
+                var compilation = CSharpCompilation.Create(
+                    assemblyName,
+                    syntaxTrees: new[] {syntaxTree},
+                    references: references,
+                    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+                using (var ms = new MemoryStream())
                 {
-                    ms.Seek(0, SeekOrigin.Begin);
-                    return Assembly.Load(ms.ToArray());
+                    var result = compilation.Emit(ms);
+
+                    if (!result.Success)
+                    {
+                        var failures = result.Diagnostics.Where(diagnostic =>
+                            diagnostic.IsWarningAsError ||
+                            diagnostic.Severity == DiagnosticSeverity.Error);
+
+                        throw new Exception(string.Join(",\n", failures));
+                    }
+                    else
+                    {
+                        ms.Seek(0, SeekOrigin.Begin);
+                        return Assembly.Load(ms.ToArray());
+                    }
                 }
-            }
+            });
         }
     }
 }
