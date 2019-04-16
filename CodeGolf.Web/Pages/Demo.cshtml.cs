00ad62089a5f3da3e89e07d09123ee0f7c537b2e
Matthew McCorry <mccorry@gmail.com>
2019-04-16 21:13:46 +0100

    Added demo site

---
+++ CodeGolf.Web/Pages/Demo.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using CodeGolf.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeGolf.Web.Pages
+{
+    public class DemoModel : PageModel
+    {
+        private readonly ICodeGolfService codeGolfService;
+
+        [BindProperty]
+        public string Code { get; set; }
+        public int Score { get; private set; }
+
+        public DemoModel(ICodeGolfService codeGolfService)
+        {
+            this.codeGolfService = codeGolfService;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var challenge = new Challenge<string>(new object[0], a => a == "Hello World");
+            var res = this.codeGolfService.Score(this.Code, challenge);
+            res.Match(a => this.Score = a, err => this.Errors = err);
+        }
+
+        public IReadOnlyList<string> Errors { get; private set; } = new List<string>();
+    }
+}
---
+++ CodeGolf/DiModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeGolf
+{
+    public static class DiModule
+    {
+        public static void Add(IServiceCollection collection)
+        {
+            collection.AddTransient<ICodeGolfService, CodeGolfService>();
+        }
+    }
+}
