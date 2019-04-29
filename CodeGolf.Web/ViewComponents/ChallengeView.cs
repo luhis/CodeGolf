f60f1987c43809f8b795b6796fe0e93f86f206fa
Matthew McCorry <mccorry@gmail.com>
2019-04-29 23:23:43 +0100

    Added view component

---
+++ CodeGolf.Web/ViewComponents/ChallengeView.cs
@@ -0,0 +1,10 @@
+﻿using CodeGolf.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeGolf.Web.ViewComponents
+{
+    public class ChallengeView : ViewComponent
+    {
+        public IViewComponentResult Invoke(Challenge<string> p) => this.View(p);
+    }
+}
