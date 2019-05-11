490c9f28e2e29f9be9896d54b4376e790af45218
Matthew McCorry <mccorry@gmail.com>
2019-05-11 07:44:18 +0100

    split out view component

---
+++ CodeGolf.Web/ViewComponents/ErrorsView.cs
@@ -0,0 +1,10 @@
+﻿using CodeGolf.Service.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeGolf.Web.ViewComponents
+{
+    public class ErrorsView : ViewComponent
+    {
+        public IViewComponentResult Invoke(ErrorSet p) => this.View(p);
+    }
+}
