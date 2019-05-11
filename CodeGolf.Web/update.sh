5853df1a06c8711f3fccdd9d764bb286a651d73e
Matthew McCorry <mccorry@gmail.com>
2019-05-11 07:59:11 +0100

    update deploy script

--- CodeGolf.Web/update.sh
+++ CodeGolf.Web/update.sh
@@ -1,7 +1,8 @@
-echo "update source"
+echo "Update Source"
 git pull
 echo "Stop codegolf service"
 systemctl stop kestrel-codegolf.service
+sleep 5
 echo "Run build"
 dotnet publish -o ./publish -c Release
 echo "Start codegolf service"
