3943403dcf7d7b18b172ecc2b0c07474b31163ff
Matthew McCorry <mccorry@gmail.com>
2019-05-15 20:29:27 +0100

    Move default folders

--- CodeGolf.Web/update.sh
+++ CodeGolf.Web/update.sh
@@ -4,6 +4,8 @@ echo "Stop codegolf service"
 systemctl stop kestrel-codegolf.service
 sleep 5
 echo "Run build"
-dotnet publish -o ./publish -c Release
+dotnet publish -o ../publish -c Release
+echo "Setting file ownership"
+chown www-data:www-data ../publish/wwwroot
 echo "Start codegolf service"
 systemctl start kestrel-codegolf.service
