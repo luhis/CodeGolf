90aa9cfb70db1e1aa4fdf12632ad3e50318e487c
Matthew McCorry <mccorry@gmail.com>
2019-05-27 10:01:51 +0100

    Removed unused file

--- CodeGolf.Web/ViewComponents/CodeInputView.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace CodeGolf.Web.ViewComponents
-{
-    public class CodeInputView : ViewComponent
-    {
-        public IViewComponentResult Invoke() => this.View();
-    }
-}
