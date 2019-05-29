8b85ab0397752987672acef0d3acb6bb9de1680e
Matthew McCorry <mccorry@gmail.com>
2019-05-29 22:07:56 +0100

    Cookie warning fix

---
+++ CodeGolf.Web/wwwroot/js/cookies.js
@@ -0,0 +1,14 @@
+﻿var element = document.getElementById("cookie-warning");
+var cookieName = "accepted-cookies";
+
+function hasCookie() {
+    return document.cookie.split(';').filter(function(item) {
+        return item.indexOf(cookieName + '=') >= 0;
+    }).length;
+}
+
+if (hasCookie()) {
+    element.hidden = true;
+} else {
+    document.cookie = cookieName + "=1";
+}
