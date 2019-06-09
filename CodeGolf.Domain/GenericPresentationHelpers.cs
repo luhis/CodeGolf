6acfff678ecad8ca9cd07c37a3fd96fc754781e0
Matthew McCorry <mccorry@gmail.com>
2019-06-09 20:37:58 +0100

    Improved code hints

--- CodeGolf.Domain/GenericPresentationHelpers.cs
+++ CodeGolf.Domain/GenericPresentationHelpers.cs
@@ -34,5 +34,30 @@ namespace CodeGolf.Domain
             var zipped = challenge.Args.Zip(paramTypes, Tuple.Create).Select(a => WrapIfString(a.Item1, a.Item2));
             return $"({string.Join(", ", zipped)}) => {WrapIfArray(challenge.ExpectedResult, returnType)}";
         }
+
+        private static readonly IReadOnlyDictionary<Type, string> Aliases = new Dictionary<Type, string>()
+        {
+            {typeof(string), "string" },
+            {typeof(string[]), "string[]" },
+            {typeof(int), "int" }
+        };
+
+        private static string GetAlias(Type t)
+        {
+            if (Aliases.ContainsKey(t))
+            {
+                return Aliases[t];
+            }
+            else
+            {
+                return t.Name;
+            }
+        }
+
+        public static string GetFuncTemplate(IChallengeSet set)
+        {
+            var paramPairs = set.Params.Zip(Enumerable.Range('a', 26).Select(a => (char)a), Tuple.Create).Select(t =>  $"{GetAlias(t.Item1)} {t.Item2}");
+            return $"{GetAlias(set.ReturnType)} Main({string.Join(", ", paramPairs)}) {{ return ... }}";
+        }
     }
 }
