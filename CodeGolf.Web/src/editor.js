59bcb203f3e569edbae8ea1db37758f35db2c5af
Matthew McCorry <mccorry@gmail.com>
2019-06-16 11:38:52 +0100

    Added boilerplate shortcut

--- CodeGolf.Web/src/editor.js
+++ CodeGolf.Web/src/editor.js
@@ -2,9 +2,21 @@
 import 'codemirror/mode/clike/clike';
 import 'codemirror/theme/bespin.css';
 
-CodeMirror.fromTextArea(document.getElementById("Code"),
+const editor =  CodeMirror.fromTextArea(document.getElementById("Code"),
     {
         lineNumbers: true,
         matchBrackets: true,
         mode: "text/x-csharp"
     });
+
+const codeSamples = document.getElementById('codeSample');
+const codeInputs = document.getElementById('Code');
+
+if (codeSamples && codeInputs) {
+    codeSamples.addEventListener('click', () => {
+        const doc = editor.getDoc();
+        if (doc.getValue() === '') {
+            doc.setValue(codeSamples.innerText);
+        }
+    });
+}
