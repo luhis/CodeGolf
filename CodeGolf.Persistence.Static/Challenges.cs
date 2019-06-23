013f7c4b7160910758f851dbe0ed8af1a12dce52
Matthew McCorry <mccorry@gmail.com>
2019-06-23 22:01:21 +0100

    reformat

--- CodeGolf.Persistence.Static/Challenges.cs
+++ CodeGolf.Persistence.Static/Challenges.cs
@@ -5,12 +5,14 @@ namespace CodeGolf.Persistence.Static
 {
     public static class Challenges
     {
-        public static readonly ChallengeSet<string> HelloWorld = new ChallengeSet<string>("Hello World",
+        public static readonly ChallengeSet<string> HelloWorld = new ChallengeSet<string>(
+            "Hello World",
             "Write a function that returns *'Hello World'*",
             new ParamDescription[] { },
-            new[] {new Challenge<string>(new object[0], "Hello World")});
+            new[] { new Challenge<string>(new object[0], "Hello World") });
 
-        public static readonly ChallengeSet<string> AlienSpeak = new ChallengeSet<string>("English to Alien translator",
+        public static readonly ChallengeSet<string> AlienSpeak = new ChallengeSet<string>(
+            "English to Alien translator",
             @"Take a string and output the alien's language equivalent. The translation works as such:
 
 
@@ -24,66 +26,43 @@ Swap all the vowels in the word with the corresponding:
 | i | ini |
 | o | api |
 | u | iki |",
-            new[] {new ParamDescription( typeof(string), "s")},
+            new[] { new ParamDescription(typeof(string), "s") },
             new[]
-            {
-                new Challenge<string>(new object[] {"Shaun"}, "Shoboikin"),
-                new Challenge<string>(new object[] {"Java"}, "Jobovobo"),
-                new Challenge<string>(new object[] {"Hello, World!"}, "Hunullapi, Wapirld!"),
-                new Challenge<string>(new object[] {"Alan"}, "Obolobon"),
-                new Challenge<string>(new object[] {"Australia"}, "Oboikistroboliniobo"),
-            });
-
-        public static readonly ChallengeSet<string> Calculator = new ChallengeSet<string>("Basic Calculator",
-            @"You must write a program to evaluate a string that would be entered into a calculator.",
-            new[] {new ParamDescription( typeof(string), "s")},
-            new[]
-            {
-                new Challenge<string>(new object[] {"-4 + 5"}, "1"),
-                new Challenge<string>(new object[] {"-7.5 / 2.5"}, "-3"),
-                new Challenge<string>(new object[] {"-2 + 6 / 2 * 8 - 1 / 2.5 - 18"}, "-12"),
-            });
-
-        public static readonly ChallengeSetArray<string> FizzBuzz = new ChallengeSetArray<string>("FizzBuzz",
-            "Write a program that prints the integer numbers from 1 to i inclusive. But for multiples of three print \"Fizz\" instead of the number and for the multiples of five print \"Buzz\". For numbers which are multiples of both three and five print \"FizzBuzz\".",
-            new[] {new ParamDescription( typeof(int), "i")},
-            new[]
-            {
-                new ChallengeArray<string>(new object[] {10}, new[]
                 {
-                    "1",
-                    "2",
-                    "Fizz",
-                    "4",
-                    "Buzz",
-                    "Fizz",
-                    "7",
-                    "8",
-                    "Fizz",
-                    "Buzz",
-                }),
-                new ChallengeArray<string>(new object[] {20}, new[]
-                {"1",
-                    "2",
-                    "Fizz",
-                    "4",
-                    "Buzz",
-                    "Fizz",
-                    "7",
-                    "8",
-                    "Fizz",
-                    "Buzz",
-                    "11",
-                    "Fizz",
-                    "13",
-                    "14",
-                    "FizzBuzz",
-                    "16",
-                    "17",
-                    "Fizz",
-                    "19",
-                    "Buzz",
-                })
-            });
+                    new Challenge<string>(new object[] { "Shaun" }, "Shoboikin"),
+                    new Challenge<string>(new object[] { "Java" }, "Jobovobo"),
+                    new Challenge<string>(new object[] { "Hello, World!" }, "Hunullapi, Wapirld!"),
+                    new Challenge<string>(new object[] { "Alan" }, "Obolobon"),
+                    new Challenge<string>(new object[] { "Australia" }, "Oboikistroboliniobo"),
+                });
+
+        public static readonly ChallengeSet<string> Calculator = new ChallengeSet<string>(
+            "Basic Calculator",
+            @"You must write a program to evaluate a string that would be entered into a calculator.",
+            new[] { new ParamDescription(typeof(string), "s") },
+            new[]
+                {
+                    new Challenge<string>(new object[] { "-4 + 5" }, "1"),
+                    new Challenge<string>(new object[] { "-7.5 / 2.5" }, "-3"),
+                    new Challenge<string>(new object[] { "-2 + 6 / 2 * 8 - 1 / 2.5 - 18" }, "-12"),
+                });
+
+        public static readonly ChallengeSetArray<string> FizzBuzz = new ChallengeSetArray<string>(
+            "FizzBuzz",
+            "Write a program that prints the integer numbers from 1 to i inclusive. But for multiples of three print \"Fizz\" instead of the number and for the multiples of five print \"Buzz\". For numbers which are multiples of both three and five print \"FizzBuzz\".",
+            new[] { new ParamDescription(typeof(int), "i") },
+            new[]
+                {
+                    new ChallengeArray<string>(
+                        new object[] { 10 },
+                        new[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", }),
+                    new ChallengeArray<string>(
+                        new object[] { 20 },
+                        new[]
+                            {
+                                "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13",
+                                "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz",
+                            })
+                });
     }
 }
