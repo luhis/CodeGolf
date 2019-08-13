ad62cbab327a9b8e57fda06abb86862b2a4cf3ca
Matthew McCorry <mccorry@gmail.com>
2019-08-13 21:00:50 +0100

    Removed unused files

--- CodeGolf.Web/ViewComponents/AttemptsView.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace CodeGolf.Web.ViewComponents
-{
-    public class AttemptsView : ViewComponent
-    {
-        public IViewComponentResult Invoke() => this.View();
-    }
-}
