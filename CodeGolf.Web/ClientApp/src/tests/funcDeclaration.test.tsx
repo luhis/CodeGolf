09bb5362c40e0ee5cdc0c33296653e17bfc88c62
Matthew McCorry <mccorry@gmail.com>
2019-10-23 22:50:39 +0100

    more tests

--- CodeGolf.Web/ClientApp/src/tests/funcDeclaration.test.tsx
+++ CodeGolf.Web/ClientApp/src/tests/funcDeclaration.test.tsx
@@ -1,10 +1,22 @@
 
 // See: https://github.com/mzgoddard/preact-render-spy
-import { getChallengeOverView } from "../funcDeclaration";
+import { getChallengeOverView, getFunctionDeclaration } from "../funcDeclaration";
 
 describe("Func Declaration tests", () => {
     test("getChallengeOverView", () => {
         const sig = getChallengeOverView({args: [], expectedResult: ("string" as unknown as object)}, "string");
         expect(sig).toBe("() => string");
+    });
+    test("getChallengeOverView with params", () => {
+        const sig = getChallengeOverView({args: ["int", "string"], expectedResult: ("string" as unknown as object)}, "string");
+        expect(sig).toBe("(int, string) => string");
+    });
+    test("getFunctionDeclaration", () => {
+        const sig = getFunctionDeclaration({id: "id", title: "func title", description: "description", returnType: "int", params: [], challenges: [] });
+        expect(sig).toBe("int Main() { return ... ; }");
+    });
+    test("getFunctionDeclaration with params", () => {
+        const sig = getFunctionDeclaration({id: "id", title: "func title", description: "description", returnType: "int", params: [{type: "string", suggestedName: "s"}], challenges: [] });
+        expect(sig).toBe("int Main(string s) { return ... ; }");
     });
 });
