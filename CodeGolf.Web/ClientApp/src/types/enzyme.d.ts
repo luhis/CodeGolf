a41ff254a77413b173d31000333cef40382857b7
Matthew McCorry <mccorry@gmail.com>
2019-10-30 18:43:04 +0000

    cleanup types

--- CodeGolf.Web/ClientApp/src/types/enzyme.d.ts
+++ CodeGolf.Web/ClientApp/src/types/enzyme.d.ts
@@ -1,5 +1,6 @@
+// the official types conflict with PReact
 declare module "enzyme" {
     export const render = ((f: FunctionalComponent<any>) => any);
     export const shallow = ((f: FunctionalComponent<any>) => any);
-
+    export const configure = ((f: any) => undefined);
 }
--- CodeGolf.Web/ClientApp/src/types/preact-google-recaptcha.d.ts
+++
@@ -1,6 +0,0 @@
-
-declare module "preact-google-recaptcha" {
-    import { FunctionalComponent } from "preact";
-    const ReCAPTCHA: FunctionalComponent<{ sitekey: string, size: string, onChange: ((response: string) => any) }>;
-    export = ReCAPTCHA;
-}
