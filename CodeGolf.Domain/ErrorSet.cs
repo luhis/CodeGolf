2d4139133377c7e3a9840e1dbc80402b6ac1d2b0
Matthew McCorry <mccorry@gmail.com>
2019-12-26 12:40:44 +0000

    Removed unused code

--- CodeGolf.Domain/ErrorSet.cs
+++ CodeGolf.Domain/ErrorSet.cs
@@ -5,14 +5,9 @@
 
     public class ErrorSet
     {
-        public ErrorSet(IReadOnlyList<string> errors)
-        {
-            this.Errors = EnsureArg.IsNotNull(errors, nameof(errors));
-        }
-
         public ErrorSet(params string[] s)
         {
-            this.Errors = s;
+            this.Errors = EnsureArg.IsNotNull(s, nameof(s));
         }
 
         public IReadOnlyList<string> Errors { get; }
