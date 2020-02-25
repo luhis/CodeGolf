6b07d8fb3b3b75a582f14280cc41dfd63f038018
Matthew McCorry <mccorry@gmail.com>
2020-02-25 22:23:55 +0000

    removed unused code

--- CodeGolf.Web/Attributes/RecaptchaAttribute.cs
+++ CodeGolf.Web/Attributes/RecaptchaAttribute.cs
@@ -8,7 +8,7 @@
     using Microsoft.AspNetCore.Mvc.Filters;
 
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-    public class RecaptchaAttribute : Attribute, IAsyncPageFilter, IAsyncActionFilter
+    public sealed class RecaptchaAttribute : Attribute, IAsyncActionFilter
     {
         private readonly IRecaptchaVerifier recaptchaVerifier;
         private readonly IGetCaptchaValue getCaptchaValue;
@@ -19,23 +19,6 @@
             this.recaptchaVerifier = recaptchaVerifier;
             this.getCaptchaValue = getCaptchaValue;
             this.getIp = getIp;
-        }
-
-        Task IAsyncPageFilter.OnPageHandlerSelectionAsync(PageHandlerSelectedContext context) => Task.CompletedTask;
-
-        async Task IAsyncPageFilter.OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
-        {
-            if (string.Equals(context.HttpContext.Request.Method, "POST", StringComparison.InvariantCultureIgnoreCase))
-            {
-                var ip = this.getIp.GetIp(context.HttpContext);
-                var recaptcha = this.getCaptchaValue.Get(context.HttpContext.Request);
-                if (!await this.recaptchaVerifier.IsValid(recaptcha, ip))
-                {
-                    context.ModelState.AddModelError(string.Empty, "Recaptcha is invalid");
-                }
-            }
-
-            await next();
         }
 
         async Task IAsyncActionFilter.OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
