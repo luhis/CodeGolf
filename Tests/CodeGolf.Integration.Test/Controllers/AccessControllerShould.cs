e572d712ef5624be449800f264387227078df7dc
Matthew McCorry <mccorry@gmail.com>
2020-03-15 08:39:36 +0000

    added a test

--- Tests/CodeGolf.Integration.Test/Controllers/AccessControllerShould.cs
+++ Tests/CodeGolf.Integration.Test/Controllers/AccessControllerShould.cs
@@ -1,22 +1,43 @@
 ﻿namespace CodeGolf.Integration.Test.Controllers
 {
     using System.Net.Http;
+    using System.Net.Http.Headers;
     using System.Text.Json;
     using System.Threading.Tasks;
     using CodeGolf.Integration.Test.Fixtures;
+    using CodeGolf.Integration.Test.Tooling;
     using CodeGolf.Web;
     using CodeGolf.Web.Models;
     using FluentAssertions;
+    using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.AspNetCore.TestHost;
+    using Microsoft.Extensions.DependencyInjection;
     using Xunit;
 
     public class AccessControllerShould : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
         private readonly HttpClient client;
+        private readonly HttpClient authorisedClient;
 
         public AccessControllerShould(CustomWebApplicationFactory<CodeGolf.Web.Startup> fixture)
         {
-            this.client = fixture.CreateClient(new WebApplicationFactoryClientOptions() { AllowAutoRedirect = false });
+            this.client = fixture.CreateClient(new WebApplicationFactoryClientOptions() { AllowAutoRedirect = false, HandleCookies = false });
+            this.authorisedClient = fixture.WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        services
+                            .AddAuthentication(TestAuthHandler.TestAuthSchemeName)
+                            .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(TestAuthHandler.TestAuthSchemeName, options => { });
+                    });
+                })
+                .CreateClient(new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false
+                });
+
+            this.authorisedClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(TestAuthHandler.TestAuthSchemeName);
         }
 
         [Fact]
@@ -27,6 +48,16 @@
             var body = await response.Content.ReadAsStringAsync();
             var a = JsonSerializer.Deserialize<AccessDto>(body);
             a.Should().BeEquivalentTo(new AccessDto(false, false, false));
+        }
+
+        [Fact]
+        public async Task GetAccessLoggedIn()
+        {
+            var response = await this.authorisedClient.GetAsync("/api/access/getAccess/");
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            var a = JsonSerializer.Deserialize<AccessDto>(body, JsonOptions.Options);
+            a.Should().BeEquivalentTo(new AccessDto(true, false, false));
         }
 
         [Fact]
---
+++ Tests/CodeGolf.Integration.Test/Tooling/JsonOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeGolf.Integration.Test.Tooling
+{
+    using System.Text.Json;
+    using Dahomey.Json;
+
+    public static class JsonOptions
+    {
+        public static JsonSerializerOptions Options => new JsonSerializerOptions { PropertyNameCaseInsensitive = true }.SetupExtensions();
+    }
+}
